a6d8663b8a68167f19911edbd26dd4a9d91a4604
Nasreen <nasreenda14@gmail.com>
2026-07-21 19:01:11 +0400

    refactor: enhance CORS configuration

--- Backend/index.js
+++ Backend/index.js
@@ -10,38 +10,18 @@ import expenseRoute from "./routes/expenseRoute.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", userRouter);
 app.use("/api/expenses", expenseRoute);
 
-
 connectDB();
-
-// app.get("/expenses", (req, res) => {
-//   Expense.find()
-//     .then((result) => res.json(result))
-//     .catch((err) => res.json(err));
-// });
-
-// app.post("/expenses", (req, res) => {
-//   Expense.create(req.body)
-//     .then((result) => res.json(result))
-//     .catch((err) => res.json(err));
-// });
-
-// app.put("/expenses/:id", (req, res) => {
-//   Expense.findByIdAndUpdate(req.params.id, req.body, { new: true })
-//     .then((result) => res.json(result))
-//     .catch((err) => res.json(err));
-// });
-
-// app.delete("/expenses/:id", (req, res) => {
-//   Expense.findByIdAndDelete(req.params.id)
-//     .then((result) => res.json(result))
-//     .catch((err) => res.json(err));
-// });
 
 app.listen(5000, () => {
   console.log("Server is running at port 5000");
